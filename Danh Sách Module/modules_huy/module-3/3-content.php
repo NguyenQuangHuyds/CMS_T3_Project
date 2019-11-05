e32db01dbc108efbb2ade9a4b15d099da80de9af
NguyenQuangHuyds <quanghuyds10@gmail.com>
2019-11-05 09:26:40 +0700

    first commit

--- Danh Sách Module/modules_huy/module-3/3-content.php
+++ Danh Sách Module/modules_huy/module-3/3-content.php
@@ -27,17 +27,6 @@
     });
   </script>
   <style type="text/css">
-    body {  background-image: url("catalog/view/theme/bigshop/image/patterns/27.png");
-  }
-  #header .header-row {
-    padding: 20px 0px;
-  }
-  #header #logo {
-    padding-top: 20px;
-  }
-
-  @media screen and (max-width:800px) {
-  }
 </style>
 </head>
 <body class="common-home">
@@ -48,8 +37,8 @@
       </div> -->
       <div id="bigshop-banner1" class="bigshop-banner">
         <div class="row">
-          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 moderns"><a href="#"><img src="http://demo.harnishdesign.net/opencart/bigshop/v1/image/cache/catalog/demo/banners/small-banner1-410x170.jpg" alt="sample banner" title="sample banner" /></a></div>
-          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 moderns"><a href="#"><img src="http://demo.harnishdesign.net/opencart/bigshop/v1/image/cache/catalog/demo/banners/small-banner-410x170.jpg" alt="sample banner 2" title="sample banner 2" /></a></div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 moderns"><a href="#"><img src="images/cache/catalog/demo/banners/small-banner1-410x170.jpg" alt="sample banner" title="sample banner" /></a></div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 moderns"><a href="#"><img src="images/cache/catalog/demo/banners/small-banner-410x170.jpg" alt="sample banner 2" title="sample banner 2" /></a></div>
         </div>
       </div>
     </div>
